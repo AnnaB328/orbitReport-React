d8ababedfc4655e6b6083a47265bc3b095cb0f25
AnnaB328 <annabsmith328@gmail.com>
2023-07-24 22:58:49 -0500

    Made correction to pass test

--- src/components/Table.js
+++ src/components/Table.js
@@ -14,7 +14,7 @@ const Table = ({ sat }) => {
         {sat.map((data) => (
           <tr key={data.id}>
             <td>{data.name}</td>
-            <td>{data.satelliteType}</td>
+            <td>{data.type}</td>
             <td>{data.launchDate}</td>
             <td>{data.operational ? "Active" : "Inactive"}</td>
           </tr>
