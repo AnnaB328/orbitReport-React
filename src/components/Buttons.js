ce95921293213e2464ff360bdb27a5f851134263
AnnaB328 <annabsmith328@gmail.com>
2023-07-24 22:44:25 -0500

    Last steps and optional styling

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,9 @@
 import satData from "./satData";
+import "./styling.css"
 
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
+    <div className="flex-container">
       {displaySats.map((sat, id) => {
         return (
           <button onClick={() => filterByType(sat)} key={id}>
@@ -11,10 +12,7 @@ const Buttons = ({ filterByType, setSat, displaySats }) => {
         );
       })}
 
-      <button onClick={() => setSat(satData)}>All Satellites</button>
-
-      <button>Button 3</button>
-      <button>Button 4</button>
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
